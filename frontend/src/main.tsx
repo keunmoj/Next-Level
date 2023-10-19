02389cae546e45a49bbaa965e6994a6869de5855
정훈석 <wjdgnstjr45@naver.com>
2023-10-19 16:02:42 +0900

    🎨 axios 추가 및 베이스 코드 작성, Nav 및 Router 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import theme from '@/styles/DefaultTheme';
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import theme from "@/styles/DefaultTheme";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
