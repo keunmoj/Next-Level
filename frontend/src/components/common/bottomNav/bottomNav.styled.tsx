e72bfcec23fd418b9ec6ad53dec0a021268d32e6
alsuddlek1 <minyoung99311@gmail.com>
2023-10-23 16:53:31 +0900

    :sparkles: BottomNav 기능 구현
    bottomNav 기능 구현, CSS 구현전

--- frontend/src/components/common/bottomNav/bottomNav.styled.tsx
+++ frontend/src/components/common/bottomNav/bottomNav.styled.tsx
@@ -2,8 +2,23 @@ import styled from "styled-components";
 
 const StyledBottomNav = styled.div`
     border: 1px solid red;
+    display: flex;
+    justify-content: space-between;
+`
+
+const StyledBottomNavBox = styled.div`
+    border: 1px solid blue;
+    width: 25%;
+    display: flex;
+    justify-content: center;
+`
+
+const StyledBottomNavIcon = styled.img.attrs<any>((props) => ({
+}))`
+    height: 5vh;
+    margin: 1vw;
 `
 
 export {
-    StyledBottomNav
+    StyledBottomNav, StyledBottomNavBox, StyledBottomNavIcon
 }
--- frontend/src/components/common/bottomNav/index.tsx
+++ frontend/src/components/common/bottomNav/index.tsx
@@ -1,10 +1,36 @@
-import React from "react";
-import {StyledBottomNav} from "./bottomNav.styled"
+import React, { useEffect, useState } from "react";
+import {StyledBottomNav, StyledBottomNavBox, StyledBottomNavIcon} from "./bottomNav.styled"
+import { useNavigate } from "react-router-dom";
+
 
 const BottomNav = () => {
-  return <StyledBottomNav>
-    bottomNav
-  </StyledBottomNav>;
+  const navigate = useNavigate()
+
+  // 클릭한 박스의 id값의 페이지로 이동
+  const [selectBottomNav, setSelectBottomNav] = useState()
+
+  const goBottomNav = (e : any) => {
+    setSelectBottomNav(e.target.id)    
+  }
+
+  useEffect(() => {
+    navigate(`/${selectBottomNav}`)  
+  }, [selectBottomNav])
+
+  return (<StyledBottomNav>
+    <StyledBottomNavBox onClick={goBottomNav} id="sing">
+      <StyledBottomNavIcon src='/bottomNav/contentsbutton.png' alt='sing' id='sing'></StyledBottomNavIcon>
+    </StyledBottomNavBox>
+    <StyledBottomNavBox onClick={goBottomNav} id="learning">
+      <StyledBottomNavIcon src='/bottomNav/contentsbutton.png' alt='learning' id="learning"></StyledBottomNavIcon>
+    </StyledBottomNavBox>
+    <StyledBottomNavBox onClick={goBottomNav} id="ranking">
+      <StyledBottomNavIcon src='/bottomNav/contentsbutton.png' alt='ranking' id="ranking"></StyledBottomNavIcon>
+    </StyledBottomNavBox>
+    <StyledBottomNavBox onClick={goBottomNav} id="mypage">
+      <StyledBottomNavIcon src='/bottomNav/contentsbutton.png' alt='mypage' id="mypage"></StyledBottomNavIcon>
+    </StyledBottomNavBox>
+  </StyledBottomNav>);
 };
 
 export default BottomNav;
