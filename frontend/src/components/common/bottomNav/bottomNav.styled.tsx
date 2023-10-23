c285ae343576178efd637c477ceb03172190d457
alsuddlek1 <minyoung99311@gmail.com>
2023-10-23 15:38:51 +0900

    :lipstick: Index.html body 내부 속성 margin : 0px 설정
    index.html body 내부 속성 margin : 0px 설정

---
+++ frontend/src/components/common/bottomNav/bottomNav.styled.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledBottomNav = styled.div`
+    border: 1px solid red;
+`
+
+export {
+    StyledBottomNav
+}
---
+++ frontend/src/components/common/bottomNav/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {StyledBottomNav} from "./bottomNav.styled"
+
+const BottomNav = () => {
+  return <StyledBottomNav>
+    bottomNav
+  </StyledBottomNav>;
+};
+
+export default BottomNav;
