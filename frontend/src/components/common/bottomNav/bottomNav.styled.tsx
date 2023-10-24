762d4db2f3a5a33d0469141f4b8356506986403f
alsuddlek1 <minyoung99311@gmail.com>
2023-10-24 10:30:45 +0900

    :fire: TopNav 삭제
    topNav 삭제

--- frontend/src/components/common/bottomNav/bottomNav.styled.tsx
+++ frontend/src/components/common/bottomNav/bottomNav.styled.tsx
@@ -1,24 +1,21 @@
 import styled from "styled-components";
 
 const StyledBottomNav = styled.div`
-    border: 1px solid red;
-    display: flex;
-    justify-content: space-between;
-`
+  /* border: 1px solid red; */
+  display: flex;
+  justify-content: space-between;
+`;
 
 const StyledBottomNavBox = styled.div`
-    border: 1px solid blue;
-    width: 25%;
-    display: flex;
-    justify-content: center;
-`
+  border: 1px solid blue;
+  width: 25%;
+  display: flex;
+  justify-content: center;
+`;
 
-const StyledBottomNavIcon = styled.img.attrs<any>(() => ({
-}))`
-    height: 5vh;
-    margin: 1vw;
-`
+const StyledBottomNavIcon = styled.img.attrs<any>(() => ({}))`
+  height: 5vh;
+  margin: 1vw;
+`;
 
-export {
-    StyledBottomNav, StyledBottomNavBox, StyledBottomNavIcon
-}
+export { StyledBottomNav, StyledBottomNavBox, StyledBottomNavIcon };
