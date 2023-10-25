37dda2902b523d7307dc780460d11307fc0133f6
hsj4436 <hsj4436@gmail.com>
2023-10-25 18:01:28 +0900

    :fire: 사용하지 않는 import 제거

--- backend/gateway/src/main/java/com/ddoya/gateway/common/config/RedisConfig.java
+++ backend/gateway/src/main/java/com/ddoya/gateway/common/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package com.ddoya.gateway.common.config;
 
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
