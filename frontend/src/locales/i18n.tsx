5bb6cd1aaea4f768037edc5ac571937427dcbf4b
alsuddlek1 <minyoung99311@gmail.com>
2023-10-25 15:58:41 +0900

    :sparkles: I18next 기본 셋팅
    i18next 기본 셋팅

---
+++ frontend/src/locales/i18n.tsx
@@ -0,0 +1,33 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import translationEN from "./en/translation.json";
+import translationKO from "./ko/translation.json";
+
+const resources = {
+  "en-US": {
+    translation: translationEN,
+  },
+  "ko-KR": {
+    translation: translationKO,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources: resources,
+  lng: "ko", // 기본설정언어
+  fallbackLng: {
+    "en-US": ["en-US"],
+    default: ["ko-KR"],
+  }, // 번역파일에서 찾을 수 없는 경우 기본 언어
+  debug: true, //console
+  defaultNS: "translation",
+  ns: "translations",
+  interpolation: {
+    escapeValue: false,
+  },
+  react: {
+    useSuspense: false,
+  },
+});
+
+export default i18n;
