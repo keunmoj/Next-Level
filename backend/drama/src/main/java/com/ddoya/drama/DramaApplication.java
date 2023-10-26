58c6c730209491298bf1578f86665b24c2aef810
hsj4436 <hsj4436@gmail.com>
2023-10-26 12:25:17 +0900

    :tada: 프로젝트 생성

---
+++ backend/drama/src/main/java/com/ddoya/drama/DramaApplication.java
@@ -0,0 +1,15 @@
+package com.ddoya.drama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DramaApplication.class, args);
+	}
+
+}
