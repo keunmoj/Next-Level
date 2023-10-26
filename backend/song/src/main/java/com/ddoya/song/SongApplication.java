a789e80e422b89366f98c3e7267554c42bcd49e4
hsj4436 <hsj4436@gmail.com>
2023-10-26 12:18:11 +0900

    :tada: 프로젝트 생성

---
+++ backend/song/src/main/java/com/ddoya/song/SongApplication.java
@@ -0,0 +1,15 @@
+package com.ddoya.song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongApplication.class, args);
+	}
+
+}
