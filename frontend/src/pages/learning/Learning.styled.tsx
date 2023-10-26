038bcfd0c0b7ad56fb021d9fe1dc14ed8e069052
alsuddlek1 <minyoung99311@gmail.com>
2023-10-26 11:43:33 +0900

    :sparkles: 학습 메인 페이지 구도 셋팅
    학습 메인 페이지 구도 셋팅

---
+++ frontend/src/pages/learning/Learning.styled.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+const StyledLearning = styled.div`
+  border: 1px solid red;
+`;
+
+const StyledLearnTap = styled.div`
+  background-color: aliceblue;
+`;
+
+const StyledLearnTapButton = styled.div`
+  border: 1px solid blue;
+`;
+
+const StyledLearnBody = styled.div`
+  border: 1px solid orange;
+`;
+
+const StyledLearnContainer = styled.div`
+  border: 1px solid yellow;
+`;
+
+const StyledLearnMainBox = styled.div`
+  border: 1px solid black;
+`;
+
+const StyledLearnIcon = styled.div`
+  border: 1px solid pink;
+`;
+
+const StyledLearnContent = styled.div`
+  background-color: aqua;
+`;
+
+const StyledLearnTitle = styled.div`
+  border: 1px solid red;
+`;
+
+const StyledLearnText = styled.div`
+  border: 1px solid red;
+`;
+
+export {
+  StyledLearning,
+  StyledLearnTap,
+  StyledLearnTapButton,
+  StyledLearnBody,
+  StyledLearnContainer,
+  StyledLearnMainBox,
+  StyledLearnIcon,
+  StyledLearnContent,
+  StyledLearnTitle,
+  StyledLearnText,
+};
--- frontend/src/pages/learning/index.tsx
+++ frontend/src/pages/learning/index.tsx
@@ -1,5 +1,35 @@
+import {
+  StyledLearnTap,
+  StyledLearnTapButton,
+  StyledLearning,
+  StyledLearnBody,
+  StyledLearnContainer,
+  StyledLearnMainBox,
+  StyledLearnIcon,
+  StyledLearnTitle,
+  StyledLearnText,
+  StyledLearnContent,
+} from "./Learning.styled";
+
 const Learning = () => {
-  return <div>Learning</div>;
+  return (
+    <StyledLearning>
+      <StyledLearnTap>
+        <StyledLearnTapButton></StyledLearnTapButton>
+      </StyledLearnTap>
+      <StyledLearnBody>
+        <StyledLearnContainer>
+          <StyledLearnMainBox>
+            <StyledLearnIcon></StyledLearnIcon>
+            <StyledLearnContent>
+              <StyledLearnTitle></StyledLearnTitle>
+              <StyledLearnText></StyledLearnText>
+            </StyledLearnContent>
+          </StyledLearnMainBox>
+        </StyledLearnContainer>
+      </StyledLearnBody>
+    </StyledLearning>
+  );
 };
 
 export default Learning;
