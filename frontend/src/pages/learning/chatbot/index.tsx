dca686aebcf341c00949f159e0784bbdb69f0b0a
alsuddlek1 <minyoung99311@gmail.com>
2023-10-27 16:44:32 +0900

    :sparkles: 커스텀 학습 구도
    커스텀 학습 구도

--- frontend/src/pages/learning/chatbot/index.tsx
+++ frontend/src/pages/learning/chatbot/index.tsx
@@ -1,5 +1,51 @@
+import { useNavigate } from "react-router-dom";
+import {
+  StyledDirect,
+  StyledDirectTop,
+  StyledDirectChat,
+  StyledDirectAiChat,
+  StyledDirectUserChat,
+  StyledDirectBack,
+  StyledDirectAiImg,
+  StyledDirectAiChatContainer,
+  StyledDirectAiChatImg,
+  StyledDirectUserChatContainer,
+  StyledDirectInputContainer,
+  StyledDirectInput,
+  StyledDireactBottom,
+  StyledDirectButton,
+} from "./Chatbot.styled";
+
 const LearningChatbot = () => {
-  return <div>LearningChatbot</div>;
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate("/learning");
+  };
+
+  return (
+    <StyledDirect>
+      <StyledDirectTop>
+        <StyledDirectBack onClick={goBack}>←</StyledDirectBack>
+        <StyledDirectAiImg src="/chat/aiprofile.png" alt="profile" />
+      </StyledDirectTop>
+      <StyledDirectChat>
+        <StyledDirectAiChatContainer>
+          <StyledDirectAiChatImg src="/chat/aiprofile.png" alt="profile" />
+          <StyledDirectAiChat>안녕하세요! 만나서 반가워요</StyledDirectAiChat>
+        </StyledDirectAiChatContainer>
+        <StyledDirectUserChatContainer>
+          <StyledDirectUserChat>안녕! 나도 만나서 반가워!</StyledDirectUserChat>
+        </StyledDirectUserChatContainer>
+      </StyledDirectChat>
+      <StyledDireactBottom>
+        <StyledDirectInputContainer>
+          <StyledDirectInput />
+          <StyledDirectButton src="/chat/mike.png" alt="send" />
+          <StyledDirectButton src="/chat/send.png" alt="send" />
+        </StyledDirectInputContainer>
+      </StyledDireactBottom>
+    </StyledDirect>
+  );
 };
 
 export default LearningChatbot;
