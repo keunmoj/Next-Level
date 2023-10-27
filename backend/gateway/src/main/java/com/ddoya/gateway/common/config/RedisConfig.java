600fbfe717b1433b1eaa749a12615a85e0abb869
rmsah5368 <rmsah5368@naver.com>
2023-10-27 17:00:34 +0900

    :alien: Gateway 테스트

--- backend/gateway/src/main/java/com/ddoya/gateway/common/config/RedisConfig.java
+++ backend/gateway/src/main/java/com/ddoya/gateway/common/config/RedisConfig.java
@@ -26,6 +26,7 @@ public class RedisConfig {
     public RedisConfig(Environment environment) {
         this.environment = environment;
         this.host = environment.getProperty("redis.host");
+        System.out.println(environment.getProperty("redis.port"));
         this.port = Integer.valueOf(environment.getProperty("redis.port"));
         this.password = environment.getProperty("redis.password");
     }
