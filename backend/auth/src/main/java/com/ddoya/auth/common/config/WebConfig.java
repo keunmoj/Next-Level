48c29bb54e7f9c60dc3c3958aa54286188de2c65
hsj4436 <hsj4436@gmail.com>
2023-10-30 14:44:41 +0900

    :recycle: allowedOrigins 변경

--- backend/auth/src/main/java/com/ddoya/auth/common/config/WebConfig.java
+++ backend/auth/src/main/java/com/ddoya/auth/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:9001", "http://127.0.0.1:9001")
+            .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Content-Type")
             .exposedHeaders("Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
