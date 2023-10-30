9587f864b710912085b88523d1e33967742e3d57
hsj4436 <hsj4436@gmail.com>
2023-10-30 14:49:48 +0900

    :recycle: allowedOrigins 변경과 개행 수정

--- backend/auth/src/main/java/com/ddoya/auth/common/config/WebConfig.java
+++ backend/auth/src/main/java/com/ddoya/auth/common/config/WebConfig.java
@@ -10,10 +10,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173",
+                "http://localhost:9001", "http://127.0.0.1:9001", "http://k9e204.p.ssafy.io",
+                "https://k9e204.p.ssafy.io")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-            .allowedHeaders("Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Content-Type")
-            .exposedHeaders("Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+            .allowedHeaders("Authorization", "Access-Control-Allow-Origin",
+                "Access-Control-Allow-Credentials", "Content-Type")
+            .exposedHeaders("Authorization", "Access-Control-Allow-Origin",
+                "Access-Control-Allow-Credentials")
             .allowCredentials(true)
             .maxAge(3600);
     }
