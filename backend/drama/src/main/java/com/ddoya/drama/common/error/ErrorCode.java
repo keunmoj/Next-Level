890bdf070315d9cfecfacab4d4ea145dd7615b58
hsj4436 <hsj4436@gmail.com>
2023-11-01 14:24:43 +0900

    :sparkles: NotFoundException 추가 및 에러 코드 추가

--- backend/drama/src/main/java/com/ddoya/drama/common/error/ErrorCode.java
+++ backend/drama/src/main/java/com/ddoya/drama/common/error/ErrorCode.java
@@ -12,7 +12,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    INTERNAL_SERVER_ERROR(500, "서버에 문제가 생겼습니다.");
+    INTERNAL_SERVER_ERROR(500, "서버에 문제가 생겼습니다."),
+
+    DRAMA_PROBLEM_NOT_FOUND(404, "해당 문제를 찾을 수 없습니다.");
 
     private Integer status;
     private String message;
