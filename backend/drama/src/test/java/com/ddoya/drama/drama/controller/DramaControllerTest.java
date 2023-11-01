1d34e42c62471715764504838c88c14b9d3cb035
hsj4436 <hsj4436@gmail.com>
2023-11-01 11:49:21 +0900

    :white_check_mark: dto 필드 변경에 따른 테스트 수정

--- backend/drama/src/test/java/com/ddoya/drama/drama/controller/DramaControllerTest.java
+++ backend/drama/src/test/java/com/ddoya/drama/drama/controller/DramaControllerTest.java
@@ -22,7 +22,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
-@ActiveProfiles(profiles = {"prod"})
+@ActiveProfiles(profiles = {"local"})
 public class DramaControllerTest {
 
     @Autowired
@@ -45,7 +45,7 @@ public class DramaControllerTest {
                     fieldWithPath("message").type(JsonFieldType.STRING).description("결과 메시지"),
                     fieldWithPath("status").type(JsonFieldType.NUMBER).description("상태 코드"),
                     fieldWithPath("data.size").type(JsonFieldType.NUMBER).description("개수"),
-                    fieldWithPath("data.dramas.[].dramaId").type(JsonFieldType.NUMBER)
+                    fieldWithPath("data.dramas.[].id").type(JsonFieldType.NUMBER)
                         .description("드라마 아이디"),
                     fieldWithPath("data.dramas.[].title").type(JsonFieldType.STRING)
                         .description("제목")
@@ -66,20 +66,14 @@ public class DramaControllerTest {
                     fieldWithPath("message").type(JsonFieldType.STRING).description("결과 메시지"),
                     fieldWithPath("status").type(JsonFieldType.NUMBER).description("상태 코드"),
                     fieldWithPath("data.size").type(JsonFieldType.NUMBER).description("개수"),
-                    fieldWithPath("data.dramaClips.[].dramaProblemId").type(JsonFieldType.NUMBER)
+                    fieldWithPath("data.clips.[].id").type(JsonFieldType.NUMBER)
                         .description("드라마 문제 아이디"),
-                    fieldWithPath("data.dramaClips.[].title").type(JsonFieldType.STRING)
+                    fieldWithPath("data.clips.[].title").type(JsonFieldType.STRING)
                         .description("문제(클립) 제목"),
-                    fieldWithPath("data.dramaClips.[].hit").type(JsonFieldType.NUMBER)
+                    fieldWithPath("data.clips.[].hit").type(JsonFieldType.NUMBER)
                         .description("조회수"),
-                    fieldWithPath("data.dramaClips.[].startTime").type(JsonFieldType.STRING)
-                        .description("시작 시간"),
-                    fieldWithPath("data.dramaClips.[].endTime").type(JsonFieldType.STRING)
-                        .description("끝 시간"),
-                    fieldWithPath("data.dramaClips.[].image").type(JsonFieldType.VARIES)
-                        .description("썸네일"),
-                    fieldWithPath("data.dramaClips.[].videoId").type(JsonFieldType.STRING)
-                        .description("유튜브 영상 아이디")
+                    fieldWithPath("data.clips.[].image").type(JsonFieldType.VARIES)
+                        .description("썸네일")
                 )));
     }
 
@@ -97,20 +91,14 @@ public class DramaControllerTest {
                     fieldWithPath("message").type(JsonFieldType.STRING).description("결과 메시지"),
                     fieldWithPath("status").type(JsonFieldType.NUMBER).description("상태 코드"),
                     fieldWithPath("data.size").type(JsonFieldType.NUMBER).description("개수"),
-                    fieldWithPath("data.dramaClips.[].dramaProblemId").type(JsonFieldType.NUMBER)
+                    fieldWithPath("data.clips.[].id").type(JsonFieldType.NUMBER)
                         .description("드라마 문제 아이디"),
-                    fieldWithPath("data.dramaClips.[].title").type(JsonFieldType.STRING)
+                    fieldWithPath("data.clips.[].title").type(JsonFieldType.STRING)
                         .description("문제(클립) 제목"),
-                    fieldWithPath("data.dramaClips.[].hit").type(JsonFieldType.NUMBER)
+                    fieldWithPath("data.clips.[].hit").type(JsonFieldType.NUMBER)
                         .description("조회수"),
-                    fieldWithPath("data.dramaClips.[].startTime").type(JsonFieldType.STRING)
-                        .description("시작 시간"),
-                    fieldWithPath("data.dramaClips.[].endTime").type(JsonFieldType.STRING)
-                        .description("끝 시간"),
-                    fieldWithPath("data.dramaClips.[].image").type(JsonFieldType.VARIES)
-                        .description("썸네일"),
-                    fieldWithPath("data.dramaClips.[].videoId").type(JsonFieldType.STRING)
-                        .description("유튜브 영상 아이디")
+                    fieldWithPath("data.clips.[].image").type(JsonFieldType.VARIES)
+                        .description("썸네일")
                 )));
     }
 
