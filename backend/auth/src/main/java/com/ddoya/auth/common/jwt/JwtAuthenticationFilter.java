00ce627fe2cc3a03e4dff2ec0e65043bb099be50
hsj4436 <hsj4436@gmail.com>
2023-11-02 14:54:49 +0900

    :recycle: jwt 인증 필터 수정

--- backend/auth/src/main/java/com/ddoya/auth/common/jwt/JwtAuthenticationFilter.java
+++ backend/auth/src/main/java/com/ddoya/auth/common/jwt/JwtAuthenticationFilter.java
@@ -4,35 +4,38 @@ import com.ddoya.auth.common.error.ErrorCode;
 import com.ddoya.auth.common.error.exception.AuthException;
 import com.ddoya.auth.common.util.RedisService;
 import java.io.IOException;
+import java.util.Arrays;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final RedisService redisService;
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
-        FilterChain filterChain) throws IOException, ServletException {
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String[] excludePath = {"/login/oauth2/**", "/oauth2/**", "/api/auth/user/reissue",
+            "/api/auth/history/add"};
+        String path = request.getRequestURI();
+        return Arrays.stream(excludePath).anyMatch(path::startsWith);
+    }
 
-        if (((HttpServletRequest) servletRequest).getRequestURI().equals("/auth/user/reissue")) {
-            filterChain.doFilter(servletRequest, servletResponse);
-            return;
-        }
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
 
         // Request Header 에서 JWT Token 추출
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) servletRequest);
+        String token = jwtTokenProvider.resolveToken(request);
 
         // validateToken 메서드로 토큰 유효성 검사
         if (redisService.isLogout(token)) {
@@ -43,6 +46,6 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-        filterChain.doFilter(servletRequest, servletResponse);
+        filterChain.doFilter(request, response);
     }
 }
