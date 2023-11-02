ffc7d58ecd45b153f8069b6fd0d1e7fe7b74c1f5
hsj4436 <hsj4436@gmail.com>
2023-11-02 17:46:00 +0900

    :recycle: 만료된 토큰만 401이도록 에러 코드 수정

--- backend/gateway/src/main/java/com/ddoya/gateway/common/error/ErrorCode.java
+++ backend/gateway/src/main/java/com/ddoya/gateway/common/error/ErrorCode.java
@@ -13,8 +13,8 @@ import lombok.Getter;
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "서버에 문제가 생겼습니다."),
 
-    INVALID_ACCESS_TOKEN(401, "유효하지 않은 엑세스 토큰입니다."),
-    LOGOUT_ACCESS_TOKEN(401, "로그아웃된 엑세스 토큰입니다."),
+    INVALID_ACCESS_TOKEN(403, "유효하지 않은 엑세스 토큰입니다."),
+    LOGOUT_ACCESS_TOKEN(403, "로그아웃된 엑세스 토큰입니다."),
     TOKEN_EXPIRED(401, "이미 만료된 토큰입니다."),
     HEADER_NOT_FOUND(400, "인증 헤더를 찾을 수 없습니다.");
 
