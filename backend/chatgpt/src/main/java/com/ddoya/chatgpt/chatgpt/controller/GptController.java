c0b0887fca6bc4b34a8fee9f6ddbe3b1c12a307c
hsj4436 <hsj4436@gmail.com>
2023-11-03 00:03:35 +0900

    :sparkles: CORS설정을 Configuration으로 변경

--- backend/chatgpt/src/main/java/com/ddoya/chatgpt/chatgpt/controller/GptController.java
+++ backend/chatgpt/src/main/java/com/ddoya/chatgpt/chatgpt/controller/GptController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 //@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true", allowedHeaders = "*", methods = {
 //        RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS,
 //        RequestMethod.HEAD })
-@CrossOrigin("*")
+//@CrossOrigin("*")
 public class GptController {
     private final ChatService chatService;
     @PostMapping("/custom")
