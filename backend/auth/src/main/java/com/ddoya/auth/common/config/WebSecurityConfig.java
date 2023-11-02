9281a21ee14ee2e1a13d5564fce17cb61b2cf390
hsj4436 <hsj4436@gmail.com>
2023-11-02 15:05:53 +0900

    :recycle: security 설정에 uri 추가

--- backend/auth/src/main/java/com/ddoya/auth/common/config/WebSecurityConfig.java
+++ backend/auth/src/main/java/com/ddoya/auth/common/config/WebSecurityConfig.java
@@ -46,6 +46,7 @@ public class WebSecurityConfig {
         http.authorizeRequests()
             .antMatchers("/login/oauth2/**", "/oauth2/**").permitAll()
             .antMatchers("/api/auth/user/reissue").permitAll()
+            .antMatchers("/api/auth/history/add").permitAll()
             .antMatchers("/api/auth/user/add-informations").hasAuthority(Role.ROLE_GUEST.name())
             .antMatchers("/api/auth/user/**").hasAuthority(Role.ROLE_USER.name())
             .anyRequest().authenticated();
