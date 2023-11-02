1299e36da0cc5bdc3d30220d2b879fe7ac10b6f6
hsj4436 <hsj4436@gmail.com>
2023-11-02 23:46:59 +0900

    :recycle: CORS 설정 변경

--- backend/chatgpt/src/main/java/com/ddoya/chatgpt/chatgpt/controller/GptController.java
+++ backend/chatgpt/src/main/java/com/ddoya/chatgpt/chatgpt/controller/GptController.java
@@ -14,9 +14,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/chatgpt")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true", allowedHeaders = "*", methods = {
-        RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS,
-        RequestMethod.HEAD })
+//@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true", allowedHeaders = "*", methods = {
+//        RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS,
+//        RequestMethod.HEAD })
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {
+    RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS,
+    RequestMethod.HEAD })
 public class GptController {
     private final ChatService chatService;
     @PostMapping("/custom")
