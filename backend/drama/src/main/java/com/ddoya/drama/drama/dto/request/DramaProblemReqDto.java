974ffd00c7fb531176266ff97b24f1d0911b6594
hsj4436 <hsj4436@gmail.com>
2023-11-02 16:30:35 +0900

    :recycle: 기본생성자 추가

--- backend/drama/src/main/java/com/ddoya/drama/drama/dto/request/DramaProblemReqDto.java
+++ backend/drama/src/main/java/com/ddoya/drama/drama/dto/request/DramaProblemReqDto.java
@@ -5,8 +5,10 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class DramaProblemReqDto {
 
