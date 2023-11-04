6df01744b8ca51a65172e9af17a1f392968521a9
hsj4436 <hsj4436@gmail.com>
2023-11-04 20:52:59 +0900

    :recycle: RequestParam 어노테이션 추가

--- backend/drama/src/main/java/com/ddoya/drama/drama/controller/DramaController.java
+++ backend/drama/src/main/java/com/ddoya/drama/drama/controller/DramaController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -75,7 +76,7 @@ public class DramaController {
     }
 
     @GetMapping("/clips")
-    ResponseEntity<Object> getDramaClips(List<Integer> problemIds) {
+    ResponseEntity<Object> getDramaClips(@RequestParam List<Integer> problemIds) {
         DramaClipsResDto dramaClipsResDto = dramaService.getDramaClips(problemIds);
 
         return ResponseEntity.ok(dramaClipsResDto);
