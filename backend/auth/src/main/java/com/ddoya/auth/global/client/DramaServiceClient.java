a1e1ba27405835288fcdc5f0b32350c36eb8d3a6
hsj4436 <hsj4436@gmail.com>
2023-11-04 20:54:24 +0900

    :recycle: RequestParam 어노테이션 추가

--- backend/auth/src/main/java/com/ddoya/auth/global/client/DramaServiceClient.java
+++ backend/auth/src/main/java/com/ddoya/auth/global/client/DramaServiceClient.java
@@ -4,10 +4,11 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "drama-service")
 public interface DramaServiceClient {
 
     @GetMapping("/api/drama/clips")
-    ResponseEntity<Object> getDramaClips(List<Integer> problemIds);
+    ResponseEntity<Object> getDramaClips(@RequestParam List<Integer> problemIds);
 }
