016bbde1c3773148a0b61b00b0a57b620481ef6a
hsj4436 <hsj4436@gmail.com>
2023-11-06 15:30:59 +0900

    :recycle: dto 어노테이션 추가

--- backend/auth/src/main/java/com/ddoya/auth/user/dto/request/AddInformationRequestDto.java
+++ backend/auth/src/main/java/com/ddoya/auth/user/dto/request/AddInformationRequestDto.java
@@ -1,12 +1,16 @@
 package com.ddoya.auth.user.dto.request;
 
-import com.ddoya.auth.user.entity.Language;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddInformationRequestDto {
 
     @Size(min = 1, max = 8)
