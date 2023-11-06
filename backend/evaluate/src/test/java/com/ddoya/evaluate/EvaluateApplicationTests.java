25ef03aaf3d2f1ab839b52f3935ac57d28900072
rmsah5368 <rmsah5368@naver.com>
2023-11-06 17:47:24 +0900

    발음평가 서비스 도커파일 추가 및 서버 빌드

---
+++ backend/evaluate/src/test/java/com/ddoya/evaluate/EvaluateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddoya.evaluate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
