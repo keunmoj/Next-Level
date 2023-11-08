20eb4f2d6a33a23d2b01f3ea68ab71487787b928
alsuddlek1 <minyoung99311@gmail.com>
2023-11-08 10:14:36 +0900

    :art: 드라마, 예능  hook 코드 수정
    드라마, 예능  hook 코드 수정

--- frontend/src/hooks/drama/useDramaArtistClipHook.tsx
+++ frontend/src/hooks/drama/useDramaArtistClipHook.tsx
@@ -5,12 +5,12 @@ export const useDramaArtistCliptGetHook = () => {
   const [dramaAritstClip, setDramaAritstClip] = useState<any>();
   const getDramaAritstClip = async (id: any) => {
     const res = await DramaArtistClipGet(id);
-    setDramaAritstClip(res.data.data.clips);
+    setDramaAritstClip(res.data.data.problems);
   };
   const [dramaSelectArtistClip, setDramaSelectArtistClip] = useState<any>();
   const getDramaSelectAritstClip = async (id: any) => {
     const res = await DramaArtistClipGet(id);
-    setDramaSelectArtistClip(res.data.data.clips);
+    setDramaSelectArtistClip(res.data.data.problems);
   };
   return {
     dramaAritstClip,
--- frontend/src/hooks/entertainment/useEnterArtistClipHook.tsx
+++ frontend/src/hooks/entertainment/useEnterArtistClipHook.tsx
@@ -5,12 +5,12 @@ export const useEnterArtistCliptGetHook = () => {
   const [enterAritstClip, setEnterAritstClip] = useState<any>();
   const getEnterAritstClip = async (id: any) => {
     const res = await EnterArtistClipGet(id);
-    setEnterAritstClip(res.data.data.clips);
+    setEnterAritstClip(res.data.data.problems);
   };
   const [enterSelectArtistClip, setEnterSelectArtistClip] = useState<any>();
   const getEnterSelectAritstClip = async (id: any) => {
     const res = await EnterArtistClipGet(id);
-    setEnterSelectArtistClip(res.data.data.clips);
+    setEnterSelectArtistClip(res.data.data.problems);
   };
   return {
     enterAritstClip,
