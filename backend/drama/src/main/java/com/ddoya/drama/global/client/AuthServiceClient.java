4b8cfbd9673c1e5408d6dec79ac138d72e98754b
hsj4436 <hsj4436@gmail.com>
2023-11-08 17:55:51 +0900

    :recycle: 파라미터 pathvariable로 변경

--- backend/drama/src/main/java/com/ddoya/drama/global/client/AuthServiceClient.java
+++ backend/drama/src/main/java/com/ddoya/drama/global/client/AuthServiceClient.java
@@ -4,9 +4,9 @@ import com.ddoya.drama.drama.dto.request.HistoryReqDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "auth-service")
 public interface AuthServiceClient {
@@ -14,6 +14,6 @@ public interface AuthServiceClient {
     @PostMapping("/api/auth/history/add")
     ResponseEntity<Object> addProblemHistory(@RequestBody HistoryReqDto historyReqDto);
 
-    @GetMapping("/api/auth/history/recent-drama-problem")
-    ResponseEntity<Object> getRecentDramaProblemsId(@RequestParam Long userId);
+    @GetMapping("/api/auth/history/recent-drama-problem/{userId}")
+    ResponseEntity<Object> getRecentDramaProblemsId(@PathVariable Long userId);
 }
