c75fb997a6779e0c99f147ecd6a9e499150aff88
신종혁 <shinjh4903@gmail.com>
2023-11-09 14:53:32 +0900

    ✨ 녹음여부 처리

--- frontend/src/hooks/drama/useDramaResultHook.tsx
+++ frontend/src/hooks/drama/useDramaResultHook.tsx
@@ -1,12 +1,18 @@
 import DramaResultPost from "@/api/drama/DramaResultPost";
+import usePlayerStore from "@/stores/youtube/usePlayerStore";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const useDramaResultHook = () => {
+  const isRecord = usePlayerStore((state: any) => state.isRecord);
   const navigate = useNavigate();
   const postDramaResult = async (id: any) => {
-    const res = await DramaResultPost(id);
-    if (res.data.status === 200) {
+    if (isRecord) {
+      const res = await DramaResultPost(id);
+      if (res.data.status === 200) {
+        navigate(-1);
+      }
+    } else {
       navigate(-1);
     }
   };
