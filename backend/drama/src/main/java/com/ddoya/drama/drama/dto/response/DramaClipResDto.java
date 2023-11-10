e947cea0fba33adbad7c27ffcfec305e6e87f860
hsj4436 <hsj4436@gmail.com>
2023-11-10 12:19:46 +0900

    :recycle: null 필드 제거

--- backend/drama/src/main/java/com/ddoya/drama/drama/dto/response/DramaClipResDto.java
+++ backend/drama/src/main/java/com/ddoya/drama/drama/dto/response/DramaClipResDto.java
@@ -1,12 +1,15 @@
 package com.ddoya.drama.drama.dto.response;
 
 import com.ddoya.drama.drama.entity.DramaProblem;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@JsonInclude(Include.NON_NULL)
 public class DramaClipResDto {
 
     private Integer id;
