fcc381ec4eba4b8a2865a225d2bc5adf3c1b2dc9
hsj4436 <hsj4436@gmail.com>
2023-11-10 11:05:50 +0900

    :white_check_mark: 사용 설정 파일 변경

--- backend/song/src/test/java/com/ddoya/song/song/SongControllerTest.java
+++ backend/song/src/test/java/com/ddoya/song/song/SongControllerTest.java
@@ -23,7 +23,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
-@ActiveProfiles(profiles = {"local"})
+@ActiveProfiles(profiles = {"prod"})
 public class SongControllerTest {
 
     @Autowired
