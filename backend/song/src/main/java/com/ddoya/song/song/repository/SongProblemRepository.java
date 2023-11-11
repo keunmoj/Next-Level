af03ce348f2cb61c94e861064cb0b9e0a0ea1dff
eunjng5474 <eunjng5474@gmail.com>
2023-11-11 23:25:04 +0900

    :recycle: 노래 게임 완료시 hit 증가 수정

--- backend/song/src/main/java/com/ddoya/song/song/repository/SongProblemRepository.java
+++ backend/song/src/main/java/com/ddoya/song/song/repository/SongProblemRepository.java
@@ -2,7 +2,11 @@ package com.ddoya.song.song.repository;
 
 import com.ddoya.song.common.entity.SongProblem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +15,9 @@ public interface SongProblemRepository extends JpaRepository<SongProblem, Long> 
     Optional<SongProblem> findBySongProblemId(int songProblemId);
 
     List<SongProblem> findAllBySongProblemIdIn(List<Integer> problemIds);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE SongProblem s SET s.hit = s.hit + 1 WHERE s.id = :id")
+    void incrementHit(@Param("id") Integer id);
 }
