cbc5c2ec4fb5cb90a5017a781b46b55ab7807258
rmsah5368 <rmsah5368@naver.com>
2023-11-13 10:09:01 +0900

    :alien: GPT 응답 큰따옴표 제거 및 프롬프트 수정

--- backend/chatgpt/src/main/java/com/ddoya/chatgpt/chatgpt/service/ChatService.java
+++ backend/chatgpt/src/main/java/com/ddoya/chatgpt/chatgpt/service/ChatService.java
@@ -15,16 +15,16 @@ public class ChatService {
         String prompt = "사용자가 다음과 같은 대화 주제를 원합니다. '" + subject + "'. 관련된 한개의 질문을 사용자에게 해주세요.";
         String response = chatgptService.sendMessage(prompt);
         String[] parts = response.split("\n\n", 2);
-        return ChatResponseDto.builder().response(parts[1]).build();
+        return ChatResponseDto.builder().response(parts[1].replace("\"", "")).build();
     }
 
     public ChatResponseDto getNextQuestion(String subject, String request) {
         // ChatGPT 에게 과거 대화 내용에 기반한 다음 질문을 얻어옵니다.
-        String prompt = "'" + subject + "'의 주제로 대화 중입니다.'" + request + "' 다음 A의 답변을 생성해주세요";
+        String prompt = "'" + subject + "'의 주제로 대화 중입니다.'" + request + "' B의 응답에 맞는 다음 A의 답변을 생성해주세요";
         String response = chatgptService.sendMessage(prompt);
 
         String[] parts = response.split("\n\n", 2);
-        return ChatResponseDto.builder().response(parts[1].substring(3)).build();
+        return ChatResponseDto.builder().response(parts[1].substring(3).replace("\"", "")).build();
     }
 
 }
