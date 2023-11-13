08184e5235d8f62665f48607f219e1a4323c3ebd
정훈석 <wjdgnstjr45@naver.com>
2023-11-13 14:21:48 +0900

    ♻️ console delete

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,5 +1,4 @@
 self.addEventListener("install", function (e) {
-  console.log("[Service Worker] Install");
   e.waitUntil(
     caches.open("danbi").then(function (cache) {
       return cache.addAll(["/", "/index.html", "/src/"]);
@@ -10,14 +9,8 @@ self.addEventListener("install", function (e) {
 self.addEventListener("fetch", function (e) {
   e.respondWith(
     caches.match(e.request).then(function (r) {
-      console.log("[Service Worker] Fetching resource: " + e.request.url);
-      return (
-        r ||
-        fetch(e.request).then(function (response) {
+      return (r || fetch(e.request).then(function (response) {
           return caches.open("danbi").then(function (cache) {
-            console.log(
-              "[Service Worker] Caching new resource: " + e.request.url
-            );
             cache.put(e.request, response.clone());
             return response;
           });
