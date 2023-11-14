368a895ee4f0964afb3ea0bd97f55cb47e90124b
hsj4436 <hsj4436@gmail.com>
2023-11-14 10:02:52 +0900

    :recycle: vo 필드 변경 및 null일 경우 포함시키지 않음

--- backend/auth/src/main/java/com/ddoya/auth/history/vo/ProblemResVo.java
+++ backend/auth/src/main/java/com/ddoya/auth/history/vo/ProblemResVo.java
@@ -1,5 +1,7 @@
 package com.ddoya.auth.history.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,10 +9,12 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(Include.NON_NULL)
 public class ProblemResVo {
 
     private Integer id;
     private String title;
+    private String artist;
     private Integer hit;
     private String image;
 
