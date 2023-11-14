586905ab716ef6b7a99c4ad44cb3454015776668
hsj4436 <hsj4436@gmail.com>
2023-11-14 11:27:14 +0900

    :recycle: 응답에 아티스트명 추가

--- backend/auth/src/main/java/com/ddoya/auth/history/dto/response/HistoryDto.java
+++ backend/auth/src/main/java/com/ddoya/auth/history/dto/response/HistoryDto.java
@@ -3,6 +3,8 @@ package com.ddoya.auth.history.dto.response;
 import com.ddoya.auth.history.entity.History;
 import com.ddoya.auth.history.entity.ProblemType;
 import com.ddoya.auth.history.vo.ProblemResVo;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,11 +14,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(Include.NON_NULL)
 public class HistoryDto {
 
     private Integer id;
     private LocalDate date;
     private String title;
+    private String artist;
     private ProblemType problemType;
     private Integer score;
     private Integer hit;
@@ -27,6 +31,7 @@ public class HistoryDto {
         this.id = problemResVo.getId();
         this.date = history.getDate();
         this.title = problemResVo.getTitle();
+        this.artist = problemResVo.getArtist();
         this.problemType = history.getType();
         this.score = history.getScore();
         this.hit = problemResVo.getHit();
