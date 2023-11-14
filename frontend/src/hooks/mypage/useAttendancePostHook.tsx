d3ba2ec38a8f08fe2b1767fcec304e6ee14d2b85
신종혁 <shinjh4903@gmail.com>
2023-11-14 09:09:50 +0900

    🐛 출석 에러 해결

--- frontend/src/hooks/mypage/useAttendancePostHook.tsx
+++ frontend/src/hooks/mypage/useAttendancePostHook.tsx
@@ -3,11 +3,11 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const useAttendancePostHook = () => {
-  const navigate = useNavigate();
   const postAttendance = async () => {
     const res = await AttendancePost();
     if (res.data.status === 200) {
-      navigate("/mypage");
+      console.log(res.data.message);
+      window.location.reload();
     }
   };
   return { postAttendance };
