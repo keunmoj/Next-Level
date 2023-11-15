8699b0fdfc505e42d54d8727f34f2d47e0d6d6ee
alsuddlek1 <minyoung99311@gmail.com>
2023-11-15 16:20:49 +0900

    :lipstick: 랭킹css
    랭킹css

--- frontend/src/pages/ranking/Ranking.styled.tsx
+++ frontend/src/pages/ranking/Ranking.styled.tsx
@@ -23,13 +23,12 @@ const StyledRankingBody = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  /* border: 1px solid red; */
   height: 84vh;
 `;
 
 const StyleMyRankingContainer = styled.div`
   width: 100vw;
-  height: 10vh;
+  height: 15vh;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -38,45 +37,46 @@ const StyleMyRankingContainer = styled.div`
 const StyledMyRanking = styled.div`
   width: 90%;
   height: 100%;
-  /* background-color: ${(props) => props.theme.colors.main}; */
-  border-radius: 5px;
-  border: 2px solid ${(props) => props.theme.colors.main};
   display: flex;
   justify-content: space-between;
+  background-color: ${(props) => props.theme.colors.white};
+  border-radius: 5px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
 `;
 
 const StyledMyProfileContainer = styled.div`
-  /* flex: 3; */
   display: flex;
   align-items: center;
   justify-content: center;
-  /* gap: 10%; */
 `;
 
 const StyledMyProfile = styled.img`
-  width: 16vw;
-  height: 16vw;
-  border-radius: 50%;
-  /* border: 2px solid black; */
-  background-color: white;
-  margin: 1vw 5vw 1vw 5vw;
+  width: 27vw;
+  height: 27vw;
+  border-radius: 5px;
   object-fit: contain;
+  margin-left: 10px;
 `;
 
-const StyledTopScoreImg = styled.img`
+const StyledImageContainer = styled.div`
   width: 18vw;
   height: 18vw;
-  border-radius: 50%;
-  border: 4px solid ${(props) => props.theme.colors.light};
+  border-radius: 5px;
+  /* border: 4px solid ${(props) => props.theme.colors.light}; */
   padding: 3px;
   background-color: white;
   margin: 1vw 5vw 1vw 5vw;
-  object-fit: contain;
+`;
+
+const StyledTopScoreImg = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
 `;
 
 const StyledMyName = styled.div`
   font-family: ${(props) => props.theme.fonts.boldfont};
-  font-size: ${(props) => props.theme.fontsize.regular};
+  font-size: ${(props) => props.theme.fontsize.semilarge};
 `;
 
 const StyledMyTierContainer = styled.div`
@@ -90,25 +90,28 @@ const StyledMyTierContainer = styled.div`
 const StyledMyTier = styled.img.attrs<any>((props) => ({
   src: `/mypage/${props.grade}.svg`,
 }))`
-  width: 48px;
-  height: 48px;
+  width: 24px;
+  height: 24px;
 `;
 
 const StyledMyTier2 = styled.img.attrs<any>((props) => ({
   src: `/mypage/${props.grade}.svg`,
 }))`
-  width: 36px;
-  height: 36px;
+  width: 24px;
+  height: 24px;
 `;
 
+// 세로 컨테이너
 const StlyedMyScoreContainer = styled.div`
-  /* flex: 1; */
+  width: 100%;
+  height: 100%;
   display: flex;
-  align-items: center;
   justify-content: center;
   flex-direction: column;
-  margin-right: 20px;
-  /* gap: 3%; */
+  font-family: ${(props) => props.theme.fonts.semiboldfont};
+  font-size: ${(props) => props.theme.fontsize.regular};
+  margin: 10px;
+  align-items: flex-start;
 `;
 
 const StyledMyScoreTitle = styled.div`
@@ -119,38 +122,39 @@ const StyledMyScoreTitle = styled.div`
 const StyledMyScore = styled.div`
   font-family: ${(props) => props.theme.fonts.semiboldfont};
   font-size: ${(props) => props.theme.fontsize.small};
+  margin: 5px;
+  width: 100%;
+  text-align: center;
 `;
 
 const StyledAllRankingContainer = styled.div`
-  /* height: 65vh; */
   width: 90vw;
-  /* background-color: ${(props) => props.theme.colors.white}; */
-  /* position: absolute; */
-  /* bottom: 6vh; */
-  /* left: 50%; */
-  /* transform: translate(-50%); */
-  /* border-top-right-radius: 20px; */
-  /* border-top-left-radius: 20px; */
 `;
 
 const StyledTopRankerContainer = styled.div`
   display: flex;
   justify-content: space-evenly;
-  height: 15vh;
+  /* height: 15vh; */
 `;
 
 // top 3 컨테이너
 const StyledTopRankerCard = styled.div.attrs<any>(() => {})`
+  border: 1px solid ${(props) => props.theme.colors.gray};
+  border-radius: 5px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100%;
-  width: 30%;
-  /* background-color: ${(props) => props.theme.colors.light}; */
+  width: 24vw;
   transform: ${(props) =>
-    props.index === 1 ? "translateY(-50%)" : " translateY(-20%)"};
-  /* border-radius: 50%; */
+    props.index === 0
+      ? "translateY(-10%)"
+      : props.index === 1
+      ? "translateY(-30%)"
+      : props.index === 2
+      ? "translateY(-5%)"
+      : "translateY(0%)"};
 `;
 
 const StyledTable = styled.table`
@@ -173,7 +177,7 @@ const StyledTableRow = styled.tr`
 `;
 const CustomTableRow = styled(StyledTableRow)`
   display: grid;
-  grid-template-columns: 1fr 4fr 2fr;
+  grid-template-columns: 1fr 4fr 1fr;
 `;
 
 const StyledContentContainer = styled.tbody`
@@ -198,8 +202,6 @@ const StyledTitle = styled.th`
   padding: 1vh 2vw;
   justify-content: center;
   align-items: center;
-  /* color: ${(props) => props.theme.colors.main}; */
-  /* background-color: ${(props) => props.theme.colors.menu}; */
   border-right: 2px solid ${(props) => props.theme.colors.white};
   &:last-child {
     border-right: none;
@@ -208,8 +210,9 @@ const StyledTitle = styled.th`
 const StyledMyProfile2 = styled.img`
   width: 36px;
   height: 36px;
-  border-radius: 50%;
-  border: 1px solid black;
+  border-radius: 5px;
+  background-color: aliceblue;
+  /* border: 1px solid black; */
 `;
 const StyledProfileContent = styled.div`
   width: 100%;
@@ -260,4 +263,5 @@ export {
   StyledProfileName,
   StyledRankingBody,
   StyledTopScoreImg,
+  StyledImageContainer,
 };
